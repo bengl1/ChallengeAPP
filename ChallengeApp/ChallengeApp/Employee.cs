28cf413e9996e8cb38981bc0e254386308a7310b
BG <HP@DESKTOP-Bernard>
2023-11-01 00:22:58 +0100

    Zadanie dzień 13-zmiana

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -54,27 +54,23 @@ namespace ChallengeApp
 
         public void AddGrade(char grade)
         {
+            grade = char.ToUpper(grade);
             switch (grade) 
             {
                 case 'A':
-                case 'a':
-                    this.grades.Add(100);
+                    this.AddGrade(100);
                     break;
                 case 'B':
-                case 'b':
-                    this.grades.Add(80);
+                    this.AddGrade(80);
                     break;
                 case 'C':
-                case 'c':
-                    this.grades.Add(60);
+                    this.AddGrade(60);
                     break;
                 case 'D':
-                case 'd':
-                    this.grades.Add(40);
+                    this.AddGrade(40);
                     break;
                 case 'E':
-                case 'e':
-                    this.grades.Add(20);
+                    this.AddGrade(20);
                     break;
                 default:
                     throw new Exception("Wrong Letter");
